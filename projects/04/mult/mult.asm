31d12c283a29776a744f326a3b21018914abafa3
Neel Parikh <nnkparikh@gmail.com>
2017-09-04 15:03:43 -0600

    multiplication program completed in hack assembly

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,31 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@R1 	// put R1 in a temp memory location i
+D=M
+@i
+M=D
+
+@R2		// initialize result R2 to zero
+M=0
+
+
+(LOOP)	// summation of R0, R1 times
+@i
+D=M
+@END
+D;JEQ
+@R0
+D=M
+@R2
+M=M+D
+@i
+M=M-1
+@LOOP
+0;JMP
+
+(END)
+@END	// infinite loop
+0;JMP
+
