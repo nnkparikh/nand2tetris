39d87b9140f89f949ff82cbf4e4a4115ad02285d
Neel Parikh <nnkparikh@gmail.com>
2018-09-10 14:43:01 -0600

    write translation output to a .hack text file.

--- projects/06/assembler/assembler.py
+++ projects/06/assembler/assembler.py
@@ -4,6 +4,7 @@ cmdTypes = ["","A_CMD", "C_CMD", "L_CMD"]
 def parseAssembly(file_name):
 	parser = Parser(file_name)
 	line_num = 0
+	bin_file = open("test.hack", "w")
 	while True:
 		current_cmd = parser.advance()
 		if (current_cmd == ""): break
@@ -11,7 +12,9 @@ def parseAssembly(file_name):
 		print("({}) [current_cmd: {}] [cmd_type: {}]".format(line_num, current_cmd, cmdTypes[int(cmd_type)]))
 		cmd_obj = parser.commandObj()
 		cmd_binary = cmd_obj.binaryCode()
+		bin_file.write(cmd_binary+"\n")
 		line_num += 1
-		
+	bin_file.close()
+
 if (__name__ == "__main__"):
-	parseAssembly('MaxL.asm')
+	parseAssembly('RectL.asm')
